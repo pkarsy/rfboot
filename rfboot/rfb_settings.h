ca1036f50fdc4f9b7334a42e1ddbfb554af94a65
Panagiotis Karagiannis <pkarsy@users.noreply.github.com>
2017-04-03 13:07:42 +0300

    Update rfb_settings.h

--- rfboot/rfb_settings.h
+++ rfboot/rfb_settings.h
@@ -1,14 +1,8 @@
-// If you uncomment this rfboot will generate the IV using entropy
-// from the watchdog. In this case the EPPROM is not used at all
-// but the bootloader delays for 2sec to collect the entropy
-// My tests show that with Crystal or resonator there is no enough entropy
-// Only on internal RC oscillator the method seems to work very well, because
-// it has a lot of jitter
-// #define USE_ENTROPY
 
-const uint8_t RFBOOT_CHANNEL = 0;
+const uint8_t RFBOOT_CHANNEL = 4;
 const uint8_t RFB_SYNCWORD[] = {54, 123};
 // Note XTEAKEY is only used when updating firmware. The application code does not use it
-// Note also that there is no any guarantee that the encryption offers any confidenciality
+// This specific key is useless of course. Use "rftool create ProjectName" and rftool
+// will generate new strong values for all the settings
 const uint32_t XTEAKEY[] = {1u, 2u, 3u, 4u};
 
