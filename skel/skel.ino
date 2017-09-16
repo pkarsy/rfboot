8f8c3775e70c514652e7bebb7293b2dac709b404
Panagiotis Karagiannis <pkarsy@gmail.com>
2017-09-17 00:43:15 +0300

    Update

--- skel/skel.ino
+++ skel/skel.ino
@@ -1,7 +1,8 @@
 /*
     This file is part of rfboot
     https://github.com/pkarsy/rfboot
-    It is given to the Public Domain
+    and it is given to the Public Domain. This means you can do
+    anything with it, including removing this notice.
 */
 
 #include <avr/wdt.h>
@@ -18,16 +19,24 @@ mCC1101 rf;
 void cc1101_interrupt(void) {
     // Becomes true when a packet is received
     // or after a packet is transmitted
+    // "interrupt" variable is implemented inside mCC1101 class
+    // getPacket and sendPacket can manipulate it if necessary
     rf.interrupt = true;
 }
 
 void setup() {
     rf.init();
-    // CFREQ_433 is the default
+    // CFREQ_433 is the default with mCC1101
+    // note that this is different with PanStamp library
     // rf.setCarrierFreq(CFREQ_433);
     rf.setChannel(APP_CHANNEL);
     rf.setSyncWord(APP_SYNCWORD[0],APP_SYNCWORD[1]);
     rf.disableAddressCheck();
+
+    // with the default register settings of the library
+    // CC1101 chip asserts gdo0 LOW when a packet received
+    // or after a packet is sent.
+    // gdo1 is not used.
     attachInterrupt(0, cc1101_interrupt, FALLING);
 
     // Uncomment to use a LED in A5
@@ -37,14 +46,32 @@ void setup() {
 void loop() {
     // rfboot bootloader already enables watchdog timer
     // we must reset it periodically
+    // if an operation/function inside loop
+    // blocks for more than 2sec. the module will reset
+    // by the watchdog automatically.
     wdt_reset();
 
+    // this block of code is for demonstation purposes
+    // and also a hint that at least cc1101 module
+    // is correctly wired and atmega328 correctly powered (less certain)
+    // remove it after the initial setup, to continue with your application.
     {
-        const int DELAY = 1000;
-        static uint32_t timer = millis();
-        if (millis()-timer>=DELAY) {
-            PRINTLN("timer = %lu",timer);
-            timer+=DELAY;
+        // we print a message every 1000ms=1sec
+        // the clock can be off by a few % because
+        // RC oscillator is not very accurate
+        const unsigned int DELAY = 1000;
+        static uint32_t old_uptime_ms = 0;
+        uint32_t current_uptime_ms = millis();
+        if (current_uptime_ms - old_uptime_ms >= DELAY) {
+            // Generally avoid to print a lot of messages, as the link is half duplex.
+            // PRINT and PRINTLN use the F() operator, so the string literals do not
+            // consume precious RAM. It is equivalent to
+            // rf.print(F("formatString"), arg1, arg2, ..)
+            PRINTLN("uptime = %lu sec", current_uptime_ms/DELAY);
+            // we could just write
+            // old_uptime_ms = current_uptime_ms
+            // but we want it to be multiple of 1000
+            old_uptime_ms = current_uptime_ms / DELAY*DELAY;
         }
     }
 
@@ -65,11 +92,7 @@ void loop() {
             }
             // here you can put code to check for any input
             if (pkt_size==1 and packet[0]>='0' and packet[0]<='9') {
-                // This is a test. Generally do not overdo it with messages, as the link is
-                // half duplex.
-                // PRINT and PRINTLN use the F() operator, so the string literals do not
-                // consume precious RAM. It is equivalent to
-                // rf.print(F("formatString"), arg1, arg2, ..)
+
                 PRINTLN("Key %c detected", packet[0]);
             }
         }
